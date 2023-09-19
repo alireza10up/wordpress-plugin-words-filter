d495aa0786bcabedac1514c506f45d8a2aa4429b
pc-alireza10up <toolsalireza10up@gmail.com>
2023-09-19 10:24:04 +0330

    Basic structure

---
+++ words-filter.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name:       words filter
+ * Description:       A simple plugin for filtering words in WordPress
+ * Version:           1.0.0
+ * Author:            alireza10up
+ * Author URI:        https://www.alireza10up.ir/
+ * License:           GPL-2.0+
+ * License URI:       MIT
+ */
+
+define('WF_DIR_PATH', plugin_dir_path(__FILE__));
+define('WF_DIR_URL', plugin_dir_url(__FILE__));
+define('WF_DIR_INC', WF_DIR_PATH . '/inc/');
