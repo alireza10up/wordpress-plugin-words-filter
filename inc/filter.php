b4006382279974e04461faa269428ab8ed1263c0
pc-alireza10up <toolsalireza10up@gmail.com>
2023-09-19 11:24:20 +0330

    Compatibility with Persian texts

--- inc/filter.php
+++ inc/filter.php
@@ -3,8 +3,7 @@
 add_filter('the_content', function ($content) {
     $words = get_option('words_filter');
     foreach ($words as $word) {
-        $repace = str_repeat('*', strlen($word));
-        $content = preg_replace("/{$word}/", $repace, $content);
+        $content = preg_replace("/{$word}/", str_repeat('*', mb_strlen($word)), $content);
     }
     return $content;
 });
